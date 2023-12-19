3407682b38bde3fb47d90d709efb09fbdcc411d9
Rameez Ali <rameez.ali@stackone.co>
2023-12-19 23:59:32 +0500

    Basic steup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  fs.readFile("./files/arts.json", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("File read failed.", err);
+      return res.status(500).send("Error reading file");
+    }
+
+    try {
+      const fileData = JSON.parse(jsonString);
+      res.render("index", { fileData });
+    } catch (error) {
+      console.log("Error parsing JSON:", error);
+      return res.status(500).send("Error parsing JSON");
+    }
+  });
+});
+
+app.get("/postIllustration", async (req, res) => {});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
